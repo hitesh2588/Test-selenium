1bc28a6641a4f9a008cc282e12d85205cec813e8
Hitesh <Hitesh@Hitesh-PC>
2016-09-10 00:41:08 +0530

    Signed-off-by: Hitesh <Hitesh@Hitesh-PC>

--- src/seleniumtest/banking.java
+++ src/seleniumtest/banking.java
@@ -1,10 +1,57 @@
 package seleniumtest;
 
-public class banking {
+import org.openqa.selenium.WebDriver.Options;
+import org.openqa.selenium.WebDriver.Window;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class banking {
+	
+	//Calculator calc=new Calculator();
+	
+	public void profit(){
+		
+	}
+
+	public void loss(){
+		
+	}
+	
+	public void addSalary(){
+		
+		//calc.add();
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		banking b= new banking();
+		b.addSalary();
+		
+		/*DefaultSelenium selenium= new DefaultSelenium();
+		selenium.start();
+		selenium.open("http://google.com");
+		selenium.windowMaximize();*/
+		
+		
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://google.com");
+		
+		String title=driver.getTitle();
+		
+		title.length();
+		
+		System.out.println(driver.getTitle().length());
+		
+		System.out.println(title.length());
+		//driver.manage().window().maximize();
+		
+		Options opt = driver.manage();
+		Window win = opt.window();
+		win.maximize();
+		
 	}
 
 }
+
+
